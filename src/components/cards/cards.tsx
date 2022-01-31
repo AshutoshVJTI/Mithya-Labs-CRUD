c986273f9fb2ab01ac3606dbaa9757bbc2e04cf9
AshutoshVJTI <akmathore_20@mc.vjti.ac.in>
2022-01-31 11:51:30 +0530

    Initial Commit

---
+++ src/components/cards/cards.tsx
@@ -0,0 +1,59 @@
+import "./cards.css";
+import Card from '@mui/material/Card';
+
+interface CardProps {
+    data: {
+        name: string,
+        gender: string,
+        age: string,
+        id: number,
+    }[],
+    updater: (id: number) => void,
+    deleter: (id: number) => void,
+}
+
+const Cards = (props: CardProps) => {
+  const fillForm = (val: any) => {
+    props.updater(val.id);
+  };
+  const deleteCard = (val: any) => {
+    props.deleter(val.id);
+  };
+
+  return (
+    <>
+      <div>
+        {props.data
+          .slice(0)
+          .reverse()
+          .map((val: any) => {
+            return (
+              <Card variant="outlined" id="singleCard">
+              <div id="cards">
+                <span>Name: {val.name}</span>
+                <span>Gender: {val.gender}</span>
+                <span>Age: {val.age}</span>
+                <div>
+                  <input
+                    id="cardbuttons"
+                    type="button"
+                    value="update"
+                    onClick={() => fillForm(val)}
+                  />
+                  <input
+                    id="cardbuttons"
+                    type="button"
+                    value="delete"
+                    onClick={() => deleteCard(val)}
+                  />
+                </div>
+              </div>
+                </Card>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default Cards;
---
+++ src/components/input/input.tsx
@@ -0,0 +1,174 @@
+import { useState } from "react";
+import "./input.css";
+// @ts-ignore
+import Cards from "../cards/cards.tsx";
+import { TextField } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+
+interface User {
+  name: string;
+  gender: string;
+  age: string;
+  id: number;
+}
+
+const gender = [
+  {
+    value: "Male",
+  },
+  {
+    value: "Female",
+  },
+  {
+    value: "Other",
+  },
+];
+
+const Input = () => {
+  const [user, setUser] = useState<User[]>([]);
+  const [userInput, setUserInput] = useState({
+    name: "",
+    gender: "Male",
+    age: "",
+    id: 1,
+  });
+  const [currId, setCurrId] = useState(0);
+  const [showUpdateButton, setShowUpdateButton] = useState(false);
+
+  const handleSubmit = () => {
+    setUser((oldUser: User[]) => [...oldUser, userInput]);
+    clearForm();
+  };
+
+  const clearForm = () => {
+    setUserInput({
+      name: "",
+      gender: "Male",
+      age: "",
+      id: userInput.id + 1,
+    });
+    (document.getElementById("input-select") as HTMLInputElement).value =
+      "Male";
+  };
+
+  const handleUpdateSubmit = () => {
+    const idx: number = currId;
+    const person: any = user.find((p: any) => p.id === idx);
+    person.name = userInput.name;
+    person.gender = userInput.gender;
+    person.age = userInput.age;
+    person.id = idx;
+    const persons: any = user.filter((p: any) => p.id !== idx);
+    persons.splice(idx - 1, 0, person);
+    setUser(persons);
+    setShowUpdateButton(false);
+    clearForm();
+  };
+
+  const fillForm = (id: number) => {
+    setShowUpdateButton(true);
+    const person: any = user.find((p: any) => p.id === id);
+    setUserInput({
+      name: person.name,
+      gender: person.gender,
+      age: person.age,
+      id: person.id,
+    });
+    setCurrId(id);
+    (document.getElementById("input-select") as HTMLInputElement).value =
+      person.gender;
+  };
+
+  const deleteCard = (id: number) => {
+    const otherCards = user.filter((p: any) => p.id !== id);
+    setUser(otherCards);
+  };
+
+  return (
+    <>
+      <div id="wrapper">
+        <form name="form1">
+          <div>
+            <TextField
+              label="Enter your name"
+              type="string"
+              variant="filled"
+              required
+              value={userInput.name}
+              onChange={(e) =>
+                setUserInput({
+                  name: e.target.value,
+                  gender: userInput.gender,
+                  age: userInput.age,
+                  id: userInput.id,
+                })
+              }
+            />
+          </div>
+          <div>
+            <TextField
+              id="outlined-select-currency-native"
+              select
+              label="Select Gender"
+              value={userInput.gender}
+              required
+              onChange={(e) =>
+                setUserInput({
+                  name: userInput.name,
+                  gender: e.target.value,
+                  age: userInput.age,
+                  id: userInput.id,
+                })
+              }
+            >
+              {gender.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.value}
+                </MenuItem>
+              ))}
+            </TextField>
+          </div>
+          <div>
+            <TextField
+              label="Enter your age"
+              type="string"
+              variant="filled"
+              required
+              value={userInput.age}
+              onChange={(e) =>
+                setUserInput({
+                  name: userInput.name,
+                  gender: userInput.gender,
+                  age: e.target.value,
+                  id: userInput.id,
+                })
+              }
+            />
+          </div>
+          <div id="buttons">
+            {!showUpdateButton ? (
+              <Button variant="contained" value="Add" onClick={handleSubmit}>
+                Add
+              </Button>
+            ) : null}
+            {showUpdateButton ? (
+              <Button
+                variant="outlined"
+                value="Update"
+                onClick={handleUpdateSubmit}
+              >
+                Update
+              </Button>
+            ) : null}
+          </div>
+        </form>
+      </div>
+      <div id="Cards">
+        <Cards data={user} updater={fillForm} deleter={deleteCard} />
+      </div>
+    </>
+  );
+};
+
+export default Input;
